fd3bb64a385df9f5a6d838e1799bd52742d6dd3b
caiovncs <devcaiovinicius@gmail.com>
2023-01-07 22:43:11 -0300

    accordion refatorado

--- js/script.js
+++ js/script.js
@@ -8,9 +8,13 @@ import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import initFetchAnimais from './modules/fetch-animais.js';
+import Accordion from './modules/accordion.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
+
+const accordion = new Accordion('[data-anime="accordion"] dt');
+accordion.init();
 
 initAnimacaoScroll();
 initAccordion();
